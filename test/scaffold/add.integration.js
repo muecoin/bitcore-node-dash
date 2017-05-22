bb09f86c5c7548ce0ab62fbd4636f308ab17ac12
BBoBBG <github@empirictechs.com>
2017-05-22 21:15:35 +0300

    MUE

--- test/scaffold/add.integration.js
+++ test/scaffold/add.integration.js
@@ -25,7 +25,7 @@ describe('#add', function() {
         throw err;
       }
       fs.writeFile(
-        testDir + '/s0/s1/bitcore-node-dash.json',
+        testDir + '/s0/s1/bitcore-node-mue.json',
         JSON.stringify(startConfig),
         function(err) {
           if (err) {
@@ -90,7 +90,7 @@ describe('#add', function() {
       });
     });
 
-    it('will update bitcore-node-dash.json services', function(done) {
+    it('will update bitcore-node-mue.json services', function(done) {
       var callCount = 0;
       var oldPackage = {
         dependencies: {
@@ -130,7 +130,7 @@ describe('#add', function() {
         services: ['a', 'b', 'c']
       }, function(err) {
         should.not.exist(err);
-        var configPath = path.resolve(testDir, 's0/s1/bitcore-node-dash.json');
+        var configPath = path.resolve(testDir, 's0/s1/bitcore-node-mue.json');
         var config = JSON.parse(fs.readFileSync(configPath));
         config.services.should.deep.equal(['a','b','c']);
         done();
